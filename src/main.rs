51b8ad5f6d6319c9144adb786d34c6b033205e58
rg060507 <g016909o@student.staffs.ac.uk>
2025-11-07 22:38:44 +0000

    Have built a basic program that creates a window and outputs some messages to the console

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
 use bevy::prelude::*;
 
-fn main() {
-    
+#[derive(Component)]
+struct Person;
 
+#[derive(Component)]
+struct Name(String);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_name, greet_people).chain()))
+        .run();
+}
+
+fn hello_world() {
+    println!("Hello, World!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("David Tenant".to_string())));
+    commands.spawn((Person, Name("Matt Smith".to_string())));
+    commands.spawn((Person, Name("Peter Capaldi".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query{
+        println!("Hello, {}!", name.0);
+    }
+}
+
+fn update_name(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query{
+        if name.0 == "David Tenant"{
+            name.0 = "Dave Tents".to_string();
+            break;
+        }
+    }
 }
